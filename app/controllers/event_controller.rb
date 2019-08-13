1223b43e53a30c34d9f4379306117486fce745f5
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-08-14 01:42:31 +0200

    op

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -13,13 +13,17 @@ class EventController < ApplicationController
   end
 
   def create
-    puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-    puts params
-    puts start_date = (params[:start_date]+" "+params[:hour_date]+":00")
-    puts end_date = (params[:end_date]+" "+params[:hour_end]+":00")
-    puts "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+
+start_date = (params[:start_date]+" "+params[:hour_date]+":00")
+end_date = (params[:end_date]+" "+params[:hour_end]+":00")
+
     @event = Event.create(start_date: start_date, end_date: end_date, title: params[:title], description: params[:description], location: params[:location], price: params[:price], administrator: current_user )
+    if @event.save
     redirect_to event_path(@event.id)
+    else
+      flash.now[:notice] = "Vérifiez les informations que vous avez rentré, certaine ne rentre pas dans les critères. "
+render "new"
+    end
   end
 
   def destroy
