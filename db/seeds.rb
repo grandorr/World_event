8c654ce491ccf290cf6f3c326872471be6cd9c0f
joshuanspada@hotmail.fr <joshuanspada@hotmail.fr>
2019-08-14 02:32:11 +0200

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 Event.destroy_all
 User.destroy_all
 Attendance.destroy_all
-
+fake_title = ["Blockchain meetup","The hacking project", "Blockchain conférence", "Il ne pourront pas tous nous arrêter", "Gilet jaune acte 1000", "Festival techno", "Festival psytrance", "Séance de Yoga", "Méditation Arc en ciel Guérison", "Lancement de missile nucléaire", "Naruto runner autour de l'élysée" ]
+fake_city = ["Marseille","Toulon","Zone 51", "Paris", "Berlin", "Nice", "Monaco", "Toulouse"]
 10.times do
 admin =  User.create(first_name: Faker::Name.first_name, last_name: "lol", email: (Faker::Name.first_name+"@yopmail.com"), password: "azertyuiop", password_confirmation: "azertyuiop")
 
@@ -10,7 +11,7 @@ test = User.create(first_name: Faker::Name.first_name, last_name: "lol", email: 
 noob = User.create(first_name: Faker::Name.first_name, last_name: "lol", email: (Faker::Name.first_name+"@yopmail.com"), password: "azertyuiop", password_confirmation: "azertyuiop")
 noob1 = User.create(first_name: Faker::Name.first_name, last_name: "lol", email: (Faker::Name.first_name+"@yopmail.com"), password: "azertyuiop", password_confirmation: "azertyuiop")
 
-e = Event.create(start_date: Time.now+5.years, end_date: Time.now+6.years , title: Faker::Esport.event, description: "blablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablablblablablablablablablablablablablablablablablablablablablablablablablblabl", price: 800, location: "Marseille", administrator: admin)
+e = Event.create(start_date: Time.now+24.hours, end_date: Time.now+30.hours , title: fake_title.sample, description: "                     "+Faker::Quote.yoda+"     ", price: rand(1..1000), location: fake_city.sample, administrator: admin)
 Attendance.create(event: e,noob: noob)
 Attendance.create(event: e,noob: noob1)
 
